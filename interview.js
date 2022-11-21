f248996383a8624b8f882e31028fceb54a593850
jenishakhaigulicf <101539935+jenishakhaigulicf@users.noreply.github.com>
2022-11-21 17:23:25 +0545

    add files to github

---
+++ interview.js
@@ -0,0 +1,12 @@
+const arr =  [1,4,3,4,5,3,2,2,1,3,1,4,7,8]
+
+const uniqueArray = [...(new Set(arr))].sort()
+
+console.log(uniqueArray);
+
+
+// what is virtual DOM
+// primitive datatypes
+// bff layer
+
+
